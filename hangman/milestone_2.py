862d93733f9978bcea95ef49bba861694ba4c68f
Tiberiu Paharnicu <paharnicutiberiu@gmail.com>
2023-12-09 14:38:12 +0000

    Optimised the code

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,12 +1,20 @@
 import random
 import string
 
-word_list = ['apples','bananas','pears','cherries','lemon']
+word_list = ['apples', 'bananas', 'pears', 'cherries', 'lemon']
 alphabet_list = list(string.ascii_lowercase)
 word = random.choice(word_list)
-guess = input("Enter a single character")
-if(len(guess) >= 1 and guess in alphabet_list):
-    print("Good input")
-else:
-    raise ValueError("Input must be a single character")
-print(word)
+
+while True:
+    guess = input("Enter a single lowercase character: ").lower()
+    
+    if len(guess) == 1 and guess in alphabet_list:
+        print("Good input")
+        break
+    else:
+        print("Invalid input. Please enter a single lowercase character.")
+
+print(f"The chosen word is: {word}")
+
+
+
