5e8657f6d78d7516f2d5dd5bf64c53d990e85ddb
Tiberiu Paharnicu <paharnicutiberiu@gmail.com>
2023-12-09 16:11:28 +0000

    Milestone 3 completed

--- hangman/mileston_3.py
+++ hangman/mileston_3.py
@@ -3,14 +3,23 @@ import random
 word_list = ['apples', 'bananas', 'pears', 'cherries', 'lemon']
 word = random.choice(word_list)
 
-while True:
-    guess = input("Enter a single lowercase character: ").lower()
-    
-    if len(guess) == 1 and guess.isalpha():
-        if guess in word:
+
+            
+def check_guess(guess):  
+        if guess.lower() in word:
             print(f"{guess} is in {word}")
-            break
+            return True
         else:
             print(f"{guess} not in the word try again")
-    else:
-        print("Invalid input. Please enter a single lowercase character.")
+            return False
+
+def ask_for_input():
+    while True:
+        guess = input("Enter a character")
+        if len(guess) == 1 and guess.isalpha():
+            if check_guess(guess):
+                break
+        else:
+            print("Wrong input try again")
+
+ask_for_input()
